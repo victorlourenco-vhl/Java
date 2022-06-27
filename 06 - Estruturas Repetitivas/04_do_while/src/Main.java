2a43ff78e88bbf426fec38d2769ac3668fb66afc
Victor Lourenco <victorhlo12@hotmail.com>
2022-06-27 10:50:53 -0300

    do-while

---
+++ 06 - Estruturas Repetitivas/04_do_while/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		char opcao = 'n';
+		
+		while(opcao != 'n') {
+			System.out.println("WHILE");
+			System.out.print("Digite a temperatura em Celsius: ");
+			double c = sc.nextDouble();
+			double f = (9  * c / 5) + 32;
+			System.out.printf("Equivalente em Fhrenheit: %.1f\n", f);
+			System.out.print("Deseja repetir (s/n)? ");
+			opcao = sc.next().charAt(0);
+			
+		}
+		
+		// No DO WHILE o bloco de comando vai ser executado pelo menos uma vez
+		
+		char opcaoB = ' ';
+		
+		do {
+			System.out.println("DO WHILE");
+			System.out.print("Digite a temperatura em Celsius: ");
+			double c = sc.nextDouble();
+			double f = (9  * c / 5) + 32;
+			System.out.printf("Equivalente em Fhrenheit: %.1f\n", f);
+			System.out.print("Deseja repetir (s/n)? ");
+			opcaoB = sc.next().charAt(0);
+		}while (opcaoB != 'n');
+
+		
+		  
+		sc.close();
+	}
+
+}
