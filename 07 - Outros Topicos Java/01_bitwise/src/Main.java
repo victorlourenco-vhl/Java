4274385a4f80b8aabc17f8c0ba00bde294d1b214
Victor Lourenco <victorhlo12@hotmail.com>
2022-06-27 17:17:44 -0300

    bitwise

---
+++ 07 - Outros Topicos Java/01_bitwise/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n1 = 89;
+		int n2 = 60;
+
+		System.out.println(n1 & n2);
+		System.out.println(n1 | n2);
+		System.out.println(n1 ^ n2);
+
+		int mask = 0b00100000;
+		int n = sc.nextInt();
+
+		if ((n & mask) != 0) {
+			System.out.println("O 6° bit é 1");
+		}
+		else {
+			System.out.println("O 6° bit é 0");
+		}
+		
+		System.out.println(n & mask);
+
+		sc.close();
+	}
+
+}
