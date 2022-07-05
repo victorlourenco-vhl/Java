558092ac4964057334e3d188e486ac4f747f012e
root <root@DESKTOP-KS1681N.localdomain>
2022-07-05 16:48:13 -0300

    metodos-estaticos

---
+++ 08 - Introducao POO/07_membros_estaticos/src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.Calculator;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter radius: ");
+		double radius = sc.nextDouble();
+		
+		double c = Calculator.circumference(radius);
+		double v = Calculator.volume(radius);
+		
+		System.out.printf("Circumference: %.2f\n", c);
+		System.out.printf("Value: %.2f\n", v);
+		System.out.printf("PI: %.5f\n", Calculator.PI);
+		
+		sc.close();
+
+	}
+	
+
+}
