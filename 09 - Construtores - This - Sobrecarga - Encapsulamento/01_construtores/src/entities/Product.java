ea365942fc1302ddf2d901cd1fe6a14cd31b9c39
Victor Lourenco <victorhlo12@hotmail.com>
2022-07-06 21:20:54 -0300

    construtores

---
+++ 09 - Construtores - This - Sobrecarga - Encapsulamento/01_construtores/src/entities/Product.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class Product {
+	public String name;
+	public double price;
+	public int quantity;
+	
+	// Construtor padrão
+	public Product() {
+		
+	}
+	// Construtor personalizado 1
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	// Construtor personalizado 2
+
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+
+	public String toString() {
+		return name + ", $ " 
+					+ String.format("%.2f", price) 
+					+ ", " + quantity + " units, Total: $ "
+					+ String.format("%.2f", totalValueInStock());
+	}
+}
