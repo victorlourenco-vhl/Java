ca5f2873c9dd2a11c1e8e3f331c339c14d737883
victorlourenco-vhl <victorhlo12@hotmail.com>
2022-07-29 07:35:33 -0300

    bloco-finally

---
+++ 14 - Tratamento de Exeções/03_finally/src/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		File file = new File("c:\\teste\\in.txt");
+		Scanner sc = null;
+		
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Error opening file: " + e.getMessage());
+		}
+		finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+
+}
